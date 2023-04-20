2c70b330f6799667071efde6c7927a21abe870e4
andrianfaa <hi@anfa.my.id>
2023-04-20 09:19:33 +0000

    build files
    Signed-off-by: andrianfaa <hi@anfa.my.id>

--- lib/hooks/useDebounce.d.ts
+++ lib/hooks/useDebounce.d.ts
@@ -1,7 +1,7 @@
 /**
  * **useDebounce hook**
  * @param value the value to be debounced
- * @param delay delay debounce time
+ * @param delay delay debounce time (ex: 1000, 1000 = 1s, 500 = 0.5s, etc.)
  * @returns `value`
  */
 declare const useDebounce: <T>(value: T, delay: number) => T;
